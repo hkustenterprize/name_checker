bbacb95f3a254ee7e6a003dc31b2f6f0eba42a5f
John Lam <john.lck40@gmail.com>
2019-09-01 20:43:56 +0800

    added iterator

--- src/deserializer.rs
+++ src/deserializer.rs
@@ -5,8 +5,8 @@ fn default_spell() -> String {
     "".to_string()
 }
 
-#[derive(Debug)]
-enum NameType {
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum NameType {
     Func,
     Var,
     Macro,
@@ -30,35 +30,59 @@ pub struct IndexFile {
     pub usr2var: Vec<Entry>,
 }
 
-fn print_entry(entry: &Entry, name_type: NameType) -> () {
-    if entry.spell != "" {
-        let name: String = entry
-            .detailed_name
-            .chars()
-            .skip(entry.short_name_offset)
-            .take(entry.short_name_size)
-            .collect();
-        if !name.starts_with("anon") {
-            print!("{:?}: {}\n", name_type, name);
+pub struct EntryName<'a> {
+    pub name: &'a str,
+    pub name_type: NameType,
+}
+
+pub struct IndexFileIterator<'a> {
+    index_file: &'a IndexFile,
+    current_type: NameType,
+    index: usize,
+}
+
+impl IndexFile {
+    pub fn get_iter(&self) -> IndexFileIterator {
+        IndexFileIterator {index_file: self, current_type: NameType::Func, index: 0}
+    }
+}
+
+fn entry_valid(entry: &Entry) -> bool {
+    entry.spell != "" && !entry.detailed_name.starts_with("anon")
+}
+
+impl<'a> Iterator for IndexFileIterator<'a> {
+    type Item = EntryName<'a>;
+    fn next(&mut self) -> Option<EntryName<'a>> {
+        loop {
+            let entries: &Vec<Entry> = match &self.current_type {
+                NameType::Func  => self.index_file.usr2func.as_ref(),
+                NameType::Type  => self.index_file.usr2type.as_ref(),
+                NameType::Var   => self.index_file.usr2var.as_ref(),
+                NameType::Macro => self.index_file.usr2var.as_ref()
+            };
+            while entries.len() > self.index {
+                let entry = &entries[self.index];
+                self.index += 1;
+                if entry_valid(entry) {
+                    let name: &str = &entry
+                        .detailed_name
+                        .as_str()[entry.short_name_offset..entry.short_name_offset + entry.short_name_size];
+                    let mut t = self.current_type;
+                    if t == NameType::Var && entry.kind == 255 {
+                        t = NameType::Macro;
+                    }
+                    return Some(EntryName {name: name, name_type: t});
+                }
+            }
+            self.current_type = match &self.current_type {
+                NameType::Func  => NameType::Type,
+                NameType::Type  => NameType::Var,
+                NameType::Var   => return None,
+                NameType::Macro => return None
+            };
+            self.index = 0;
         }
     }
 }
 
-pub fn print_index(index: &IndexFile) -> () {
-    for entry in index.usr2func.iter() {
-        print_entry(&entry, NameType::Func);
-    }
-    for entry in index.usr2type.iter() {
-        print_entry(&entry, NameType::Type);
-    }
-    for entry in index.usr2var.iter() {
-        print_entry(
-            &entry,
-            if entry.kind == 255 {
-                NameType::Macro
-            } else {
-                NameType::Var
-            },
-        );
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,25 @@ extern crate serde_json;
 
 mod deserializer;
 
-use deserializer::{print_index, IndexFile};
+use deserializer::{IndexFile};
 use std::path::Path;
 use std::fs::File;
+use std::io::Write;
 
 
 fn main() {
-    let file_path = Path::new("");
+    let mut path = String::new();
+    std::io::stdout().write_all("Please input file path\n".as_bytes()).unwrap();
+    let r = std::io::stdin().read_line(&mut path);
+    if r.is_err() {
+        print!("unexpected error in stdin");
+        return;
+    }
+    let p = path.trim();
+    let file_path = Path::new(p);
     let json_file = File::open(file_path).expect("file not found");
     let index_file: IndexFile = serde_json::from_reader(json_file).expect("parsing error");
-    print_index(&index_file);
+    for entry in index_file.get_iter() {
+        print!("{}: {:?}\n", entry.name, entry.name_type);
+    }
 }
